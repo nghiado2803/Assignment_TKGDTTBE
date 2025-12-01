6a4f76b95c1d1fca6069bc3ba573182efbddbf84
nghiado2803 <nghiadvpy00194@gmail.com>
2025-12-01 08:46:33 +0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="hello">
     <h1>Hello World Component</h1>
-    <p>Nếu thấy dòng này là đã fix xong lỗi rồi đó!</p>
   </div>
 </template>
 
 <script setup lang="ts">
-// Nếu bạn không dùng gì trong script thì để trống hoặc dùng <script setup>
 </script>
 
 <style scoped>
